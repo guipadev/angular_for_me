53a432b56f7437377b614298c817ba06df7810fd
yadevom <yaormu@gmail.com>
2023-07-29 14:24:19 -0500

    Routing y lazy loading

--- Organizacion_Estructura_Proyecto_Angular/src/app/app-routing.module.ts
+++ Organizacion_Estructura_Proyecto_Angular/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // Refresh page in production, evit error
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Organizacion_Estructura_Proyecto_Angular/src/app/core/shared/shared.module.ts
+++ Organizacion_Estructura_Proyecto_Angular/src/app/core/shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  imports: [],
+  imports: [HttpClientModule],
   declarations: [],
-  exports: [],
+  exports: [HttpClientModule],
   providers: [],
 })
 export class SharedModule {
