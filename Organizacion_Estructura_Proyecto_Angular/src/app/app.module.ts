8496cc21028280655e1186f24614fda609205b5a
yadevom <yaormu@gmail.com>
2023-07-29 14:05:47 -0500

    modularizar proyecto

--- Organizacion_Estructura_Proyecto_Angular/src/app/app.module.ts
+++ Organizacion_Estructura_Proyecto_Angular/src/app/app.module.ts
@@ -5,17 +5,13 @@ import { AppComponent } from './app.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { FormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmpleadoComponent,
-    EmpleadosComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule
-  ],
+  declarations: [AppComponent, EmpleadoComponent, EmpleadosComponent],
+  imports: [BrowserModule, FormsModule, CoreModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
