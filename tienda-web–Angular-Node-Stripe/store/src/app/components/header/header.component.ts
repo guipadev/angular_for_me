c599e311273dfd7e408fff7f9f28f4e0ca14e25e
yadevom <yaormu@gmail.com>
2024-02-28 06:30:41 -0500

    x

---
+++ tienda-web–Angular-Node-Stripe/store/src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html", // Asegúrate de incluir la extensión del archivo correcta
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
